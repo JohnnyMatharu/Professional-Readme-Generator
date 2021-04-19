282dd09cd6d058d6e642f74c73e01b1e04e7f808
Johnny <jaani.matharu@gmail.com>
2021-04-19 15:53:46 -0400

    downloaded initial code and made folders locally, committing first commit before starting project

---
+++ Develop/index.js
@@ -0,0 +1,22 @@
+// it’s important to have a high-quality README for the app. This should include what the app is for, how to use the app, how to install it, how to report issues, and how to make contributions—this last part increases the likelihood that other developers will contribute to the success of the project.
+// reminder of everything that a high-quality, professional README should contain, use guide for readme: https://github.com/coding-boot-camp/potential-enigma/blob/master/readme-guide.md
+// The application will be invoked by using the following command: node index.js
+// Because this application won’t be deployed, you’ll also need to provide a link to a walkthrough video that demonstrates its functionality. 2.2.4: Screencastify Tutorial in Module 2 of the prework. You’ll need to submit a link to the video and add it to the README of your project.
+
+
+
+
+
+// TODO: Include packages needed for this application
+
+// TODO: Create an array of questions for user input
+const questions = [];
+
+// TODO: Create a function to write README file
+function writeToFile(fileName, data) {}
+
+// TODO: Create a function to initialize app
+function init() {}
+
+// Function call to initialize app
+init();

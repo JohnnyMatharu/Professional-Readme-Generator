94334b372f3dd2b9267e483f0af2a293714c58d9
Johnny <jaani.matharu@gmail.com>
2021-04-20 12:55:47 -0400

    added basic structure, ready to code

--- Develop/index.js
+++ Develop/index.js
@@ -20,3 +20,52 @@ function init() {}
 
 // Function call to initialize app
 init();
+
+
+// screen-recorder needs to go on github (link for video), for sample readme use diff folder - ReadMe Generator
+
+// Check: Include a video of the typical user flow through your application. This includes views of the prompts and the responses after their selection.
+
+// Include any other screenshots you deem necessary to help someone who has never been introduced to your application understand the purpose and function of it. This is how you will communicate to potential employers or other developers in the future what you built and why, and to show how it works.
+
+
+// Deliverables: 20%
+// Check: A sample README generated using the application must be submitted.
+// Your GitHub repository containing your application code.
+// Walkthrough Video: 27%
+// A walkthrough video that demonstrates the functionality of the README generator must be submitted and a link to the video should be included in your README file.
+// The walkthrough video must demonstrate how a user would invoke the application from the command line.
+// The walkthrough video must demonstrate how a user would enter responses to all of the prompts in the application.
+
+// The walkthrough video must demonstrate a generated README that matches the user input and has a functioning table of contents.
+// Along with video and github url, A sample README.md file for a project repository generated using your application.
+
+
+
+
+
+// GIVEN a command-line application that accepts user input, acceptance criteria
+// WHEN I am prompted for information about my application repository
+// THEN a high-quality, professional README.md is generated with the title of my project and sections entitled Description, Table of Contents, Installation, Usage, License, Contributing, Tests, and Questions
+// WHEN I enter my project title
+// THEN this is displayed as the title of the README
+// WHEN I enter a description, installation instructions, usage information, contribution guidelines, and test instructions
+// THEN this information is added to the sections of the README entitled Description, Installation, Usage, Contributing, and Tests
+// WHEN I choose a license for my application from a list of options
+// THEN a badge for that license is added near the top of the README and a notice is added to the section of the README entitled License that explains which license the application is covered under
+// WHEN I enter my GitHub username
+// THEN this is added to the section of the README entitled Questions, with a link to my GitHub profile
+// WHEN I enter my email address
+// THEN this is added to the section of the README entitled Questions, with instructions on how to reach me with additional questions
+// WHEN I click on the links in the Table of Contents
+// THEN I am taken to the corresponding section of the README
+
+
+// Hilary:
+// Challenge: creating a readme using inquirer
+// Starter code, take files out of develop folder, readme guide, command line
+// application, no html or css, readme sections :table of contents, application user
+// questions and end generate readme., when creating gitignore file choose node.js
+// to automatically include node_modules,
+// pacakge already has Package.json, just edit
+// Nom install will install all dependencies present in package.json
